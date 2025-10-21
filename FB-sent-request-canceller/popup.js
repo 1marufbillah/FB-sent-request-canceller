bc583231051bb61cb76d9b16e9a7928c1e694374
Maruf Billah <75558710+1marufbillah@users.noreply.github.com>
2025-10-22 01:51:51 +0600

    Delete FB-sent-request-canceller directory

--- FB-sent-request-canceller/popup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("runScript").addEventListener("click", async () => {
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  if (!tab) return;
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ["content.js"]
-  });
-});
